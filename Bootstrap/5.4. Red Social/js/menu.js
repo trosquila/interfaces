16712f85d18121bee2c5f7182fd81b7e628f1adb
Sergio Salinas <correoSergio@falso.com>
2026-02-03 23:41:04 +0100

    mematen

---
+++ Bootstrap/5.4. Red Social/js/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    /* Guardamos los elementos HTML para luego trabajar con ellos */
+    const boton = document.getElementById('btnMenuMovil');
+    const fondoEnlace = document.getElementById('fondoEnlace');
+    const barraLateral = document.getElementById('barraLateralIzquierda');
+
+    if(boton) {
+        /* Cuando se haga click en el boton menu */
+        boton.addEventListener('click', function(e){
+            e.preventDefault();
+            /* Añadimos o quitamos la clase CSS active
+               al fondo y la barra lateral */
+            fondoEnlace.classList.toggle('active');
+            barraLateral.classList.toggle('active');
+        });
+    }
+
+    /* Lo mismo pero cuando se pulsa el fondo del menu para cerrarlo */
+    if(fondoEnlace) {
+        fondoEnlace.addEventListener('click', function(e){
+            e.preventDefault();
+            fondoEnlace.classList.toggle('active');
+            barraLateral.classList.toggle('active');
+        });
+    }
+});
+console.log('hola');
