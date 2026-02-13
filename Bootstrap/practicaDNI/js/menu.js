5f6575c1825ab19c825bf0f00ec23dc9fd3b351f
Sergio Salinas <correoSergio@falso.com>
2026-02-13 23:20:15 +0100

    valida

--- Bootstrap/practicaDNI/js/menu.js
+++ Bootstrap/practicaDNI/js/menu.js
@@ -1,3 +1,21 @@
+//permite controlar si el formulario esta validado
+const estadoFormulario = [
+    { nombre: "tramite", validado: false },
+    { nombre: "provincia", validado: false },
+    { nombre: "oficina", validado: false },
+    { nombre: "fechaCita", validado: false },
+    { nombre: "horaCita", validado: false },
+
+    { nombre: "numDni", validado: false },
+    { nombre: "numSoporte", validado: false },
+    { nombre: "equipoExp", validado: false },
+    { nombre: "validezDni", validado: false },
+
+    { nombre: "telefono", validado: false },
+    { nombre: "email", validado: false },
+    { nombre: "terminos", validado: false },
+];
+
 document.addEventListener('DOMContentLoaded', function () {
     /* Guardamos los elementos HTML para luego trabajar con ellos */
     const boton = document.getElementById('btnMenuMovil');
@@ -6,7 +24,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const camposKeyUp = ["numDni", "numSoporte", "equipoExp", "telefono", "email"];
     const camposChange = ["tramite", "provincia", "oficina", "fechaCita", "horaCita", "validezDni", "terminos"];
-
+    //se valida aquí el btn para que nada mas entrar al formulario se bloquee
+    validarBtn();
+    
     camposKeyUp.forEach((id) => {
         const campoForm = document.getElementById(id);
         campoForm.addEventListener("keyup", validacionFormulario);
@@ -57,7 +77,6 @@ document.addEventListener('DOMContentLoaded', function () {
 // a partir de este punto se validan los campos del formulario
 function validacionFormulario() {
     const campoForm = this;
-
     if (campoForm.id === "tramite") return validarTramite(campoForm);
     if (campoForm.id === "provincia") return validarProvincia(campoForm);
     if (campoForm.id === "oficina") return validarOficina(campoForm);
@@ -73,49 +92,89 @@ function validacionFormulario() {
     if (campoForm.id === "email") return validarEmail(campoForm);
     if (campoForm.id === "terminos") return validarTerminos(campoForm);
 }
+function validarBtn() {
+    const formValido = estadoFormulario.every(element => element.validado === true);
+    console.log(formValido);
+
+    const btn = document.getElementById("btnForm");
+    console.log(btn);
+    
+
+    if (formValido) {
+        btn.classList.remove("enlaceBloqueado")
+    } else {
+        btn.classList.add("enlaceBloqueado");   
+    }
+}
 
 function setValido(campoForm) {
     campoForm.classList.remove("is-invalid");
     campoForm.classList.add("is-valid");
+    validarBtn();
 }
 
 function setInvalido(campoForm) {
     campoForm.classList.remove("is-valid");
     campoForm.classList.add("is-invalid");
+    validarBtn();
 }
 
 function validarTramite(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarProvincia(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarOficina(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarFechaCita(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarHoraCita(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarNumDni(campoForm) {
     const esValido = /^[0-9]{8}[A-Z]$/.test(campoForm.value.trim());
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
@@ -123,6 +182,10 @@ function validarNumDni(campoForm) {
 function validarNumSoporte(campoForm) {
     let esValido = campoForm.value.trim().length >= 6;
     esValido = /^[0-9]{6,}$/.test(campoForm.value);
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
@@ -130,30 +193,50 @@ function validarNumSoporte(campoForm) {
 function validarEquipoExp(campoForm) {
     let esValido = campoForm.value.trim().length >= 4;
     esValido = /^[0-9]{4,}$/.test(campoForm.value);
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarValidezDni(campoForm) {
     const esValido = campoForm.value !== "";
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarTelefono(campoForm) {
     const esValido = /^[0-9]{9}$/.test(campoForm.value.trim());
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarEmail(campoForm) {
     const esValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(campoForm.value.trim());
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
 
 function validarTerminos(campoForm) {
     const esValido = campoForm.checked === true;
+
+    const campo = estadoFormulario.find(element => element.nombre === campoForm.id);
+    campo.validado = esValido;
+
     esValido ? setValido(campoForm) : setInvalido(campoForm);
     return esValido;
 }
