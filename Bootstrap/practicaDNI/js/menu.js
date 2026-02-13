8282dd848dbcedc2f1d0a91d15c0993bce8fb36c
Sergio Salinas <correoSergio@falso.com>
2026-02-13 22:52:10 +0100

    w

--- Bootstrap/practicaDNI/js/menu.js
+++ Bootstrap/practicaDNI/js/menu.js
@@ -4,6 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const fondoEnlace = document.getElementById('fondoEnlace');
     const barraLateral = document.getElementById('barraLateralIzquierda');
 
+    const camposKeyUp = ["numDni", "numSoporte", "equipoExp", "telefono", "email"];
+    const camposChange = ["tramite", "provincia", "oficina", "fechaCita", "horaCita", "validezDni", "terminos"];
+
+    camposKeyUp.forEach((id) => {
+        const campoForm = document.getElementById(id);
+        campoForm.addEventListener("keyup", validacionFormulario);
+    });
+
+    camposChange.forEach((id) => {
+        const campoForm = document.getElementById(id);
+        campoForm.addEventListener("change", validacionFormulario);
+    });
+
     if (boton) {
         /* Cuando se haga click en el boton menu */
         boton.addEventListener('click', function (e) {
@@ -41,4 +54,106 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// a partir de este punto se validan los campos del formulario
+function validacionFormulario() {
+    const campoForm = this;
 
+    if (campoForm.id === "tramite") return validarTramite(campoForm);
+    if (campoForm.id === "provincia") return validarProvincia(campoForm);
+    if (campoForm.id === "oficina") return validarOficina(campoForm);
+    if (campoForm.id === "fechaCita") return validarFechaCita(campoForm);
+    if (campoForm.id === "horaCita") return validarHoraCita(campoForm);
+
+    if (campoForm.id === "numDni") return validarNumDni(campoForm);
+    if (campoForm.id === "numSoporte") return validarNumSoporte(campoForm);
+    if (campoForm.id === "equipoExp") return validarEquipoExp(campoForm);
+    if (campoForm.id === "validezDni") return validarValidezDni(campoForm);
+
+    if (campoForm.id === "telefono") return validarTelefono(campoForm);
+    if (campoForm.id === "email") return validarEmail(campoForm);
+    if (campoForm.id === "terminos") return validarTerminos(campoForm);
+}
+
+function setValido(campoForm) {
+    campoForm.classList.remove("is-invalid");
+    campoForm.classList.add("is-valid");
+}
+
+function setInvalido(campoForm) {
+    campoForm.classList.remove("is-valid");
+    campoForm.classList.add("is-invalid");
+}
+
+function validarTramite(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarProvincia(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarOficina(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarFechaCita(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarHoraCita(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarNumDni(campoForm) {
+    const esValido = /^[0-9]{8}[A-Z]$/.test(campoForm.value.trim());
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarNumSoporte(campoForm) {
+    let esValido = campoForm.value.trim().length >= 6;
+    esValido = /^[0-9]{6,}$/.test(campoForm.value);
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarEquipoExp(campoForm) {
+    let esValido = campoForm.value.trim().length >= 4;
+    esValido = /^[0-9]{4,}$/.test(campoForm.value);
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarValidezDni(campoForm) {
+    const esValido = campoForm.value !== "";
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarTelefono(campoForm) {
+    const esValido = /^[0-9]{9}$/.test(campoForm.value.trim());
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarEmail(campoForm) {
+    const esValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(campoForm.value.trim());
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
+
+function validarTerminos(campoForm) {
+    const esValido = campoForm.checked === true;
+    esValido ? setValido(campoForm) : setInvalido(campoForm);
+    return esValido;
+}
